7b0ce525dc542bda76d35aac571f827ef53ff5eb
vitorgsom <vitorgabrieltitos@gmail.com>
2023-06-07 08:33:51 -0400

    Codigo exemplo express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
+
+console.log("rodando em localhost:3000");
